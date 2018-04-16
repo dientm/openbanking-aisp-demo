3500dfd98c9d923d29527a56574894e26c5c8bee
hieu brian <hieubrianpham@gmail.com>
2018-04-16 17:37:07 +0700

    initial project

---
+++ src/main/java/com/five9/openbanking/aisp/AISP/Demo/AispDemoApplication.java
@@ -0,0 +1,12 @@
+package com.five9.openbanking.aisp.AISP.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AispDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AispDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/five9/openbanking/aisp/AISP/Demo/AispDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.five9.openbanking.aisp.AISP.Demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AispDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
